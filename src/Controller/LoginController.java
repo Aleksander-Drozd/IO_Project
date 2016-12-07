f8477b78047f6ab8daa60735642d71a3a36493c4
bumpMind <damian.s.ja@gmail.com>
2016-12-07 14:05:51 +0100

    Connected LoginController - LoginView

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -2,8 +2,31 @@ package Controller;
 
 import Model.LoginModel;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
 
+    @FXML
+    private TextField loginTextField;
+    @FXML
+    private PasswordField passwordField;
+
     private LoginModel loginModel;
+
+    @FXML
+    private void handleButtonLogin() {
+        System.out.println("Zalogowano!");
+    }
+
+    @FXML
+    private void handleButtonCancel() {
+        System.out.println("Zakonczono!");
+    }
+
 
 }
