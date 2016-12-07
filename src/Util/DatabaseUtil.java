752f5bf49d33341ed5c8e88caad0793f3b468f56
John <aleksander.drozd@outlook.com>
2016-12-07 22:49:07 +0100

    Prototype of DatabaseUtil

--- src/Util/DatabaseUtil.java
+++ src/Util/DatabaseUtil.java
@@ -1,5 +1,24 @@
 package Util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseUtil {
+
+    private static String dbURL = "jdbc:mysql://localhost:8080/tour_operator";
+    private static String user = "root";
+    private static String password = "";
+
+    public static Connection getConnection(){
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(dbURL, user, password);
+            return connection;
+        } catch (SQLException e) {
+        }
+
+        return null;
+    }
 }
