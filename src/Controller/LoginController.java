a9fe638094a4e21326f7d666711c10013a95917a
bumpMind <damian.s.ja@gmail.com>
2016-12-07 14:24:05 +0100

    Working LoginView without methods implementation

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -6,9 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class LoginController {
 
     @FXML
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import Controller.LoginController;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-       /* Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();*/
+        initLoginView(primaryStage);
     }
 
+    private void initLoginView(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("View/LoginView.fxml"));
+
+            BorderPane layout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(layout);
+
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
