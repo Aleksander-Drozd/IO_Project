b1bf9a569fa5874ba55beb7bc1f4fa25411e479e
bumpMind <damian.s.ja@gmail.com>
2016-12-08 18:15:59 +0100

    Added attributes to Trip POJO

--- src/POJO/Sale.java
+++ src/POJO/Sale.java
@@ -1,10 +1,38 @@
 package POJO;
 
 
+import java.util.Date;
+
 public class Sale {
 
     private Employee employee;
     private Trip trip;
     private Customer customer;
+    private Date saleDate;
+    private int quantity;
+
+    public String getLastName() {
+        return customer.getLastName();
+    }
+
+    public String getTripTitle() {
+        return trip.getTitle();
+    }
+
+    public String getSaleDate() {
+        return saleDate.toString();
+    }
+
+    public void setLastName(String lastName) {
+        customer.setLastName(lastName);
+    }
+
+    public void setTripTitle(String tripTitle) {
+        trip.setTitle(tripTitle);
+    }
+
+    public void setSaleDate() {
+        //TODO Convert String -> Date
+    }
 
 }
