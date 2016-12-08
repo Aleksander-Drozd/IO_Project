01e7fdb8b57c849f5a6619cff284b1a3d2300795
bumpMind <damian.s.ja@gmail.com>
2016-12-08 18:13:23 +0100

    Added attributes to Trip POJO

--- src/POJO/Trip.java
+++ src/POJO/Trip.java
@@ -1,5 +1,54 @@
 package POJO;
 
 
+import java.util.Date;
+
 public class Trip {
+
+    private String title;
+    private String desciption;
+    private int days;
+    private float price;
+    private Date date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
