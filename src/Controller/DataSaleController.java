37540ae687d56596e57fcd792931b310acab6327
Aleksander Drozd <aleksander.drozd@outlook.com>
2016-12-08 22:30:38 +0100

    Added  FXML references to DataSaleController

--- src/Controller/DataSaleController.java
+++ src/Controller/DataSaleController.java
@@ -1,5 +1,33 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class DataSaleController {
+
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private RadioButton maleRadioButton;
+    @FXML
+    private RadioButton femaleRadioButton;
+    @FXML
+    private DatePicker birthDatePicker;
+    @FXML
+    private TextField postCodeTextField;
+    @FXML
+    private TextField cityTextField;
+    @FXML
+    private TextField streetTextField;
+    @FXML
+    private ComboBox tripComboBox;
+    @FXML
+    private TextField quantityTextField;
+
+    
 }
