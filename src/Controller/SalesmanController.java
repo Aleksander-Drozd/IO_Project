420ae0640f779e798237ce9a1878ba56e897e6bf
bumpMind <damian.s.ja@gmail.com>
2016-12-08 17:37:35 +0100

    Added FX objects to SalesmanController with connection to SalesmanView

--- src/Controller/SalesmanController.java
+++ src/Controller/SalesmanController.java
@@ -1,11 +1,46 @@
 package Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SalesmanController implements Initializable {
+
+    @FXML
+    protected TableView salesTableView;
+    @FXML
+    protected Label salesmanLabel;
+    @FXML
+    protected Label firstNameLabel;
+    @FXML
+    protected Label lastNameLabel;
+    @FXML
+    protected Label cityLabel;
+    @FXML
+    protected Label streetLabel;
+    @FXML
+    protected Label postCodeLabel;
+    @FXML
+    protected Label phoneNumberLabel;
+    @FXML
+    protected Label tripTitleLabel;
+    @FXML
+    protected TextArea tripDescriptonTextArea;
+    @FXML
+    protected Label tripDateLabel;
+    @FXML
+    protected Label tripPrizeLabel;
+    @FXML
+    protected Label tripDaysLabel;
+    @FXML
+    protected Label saleDateLabel;
+    @FXML
+    protected Label saleCostLabel;
 
     protected ViewSalesController viewSalesController;
     protected DataSaleController dataSaleController;
