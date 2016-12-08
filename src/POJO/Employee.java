1dddf9c83088cb86d40432ef845d6a831149f2cf
bumpMind <damian.s.ja@gmail.com>
2016-12-08 13:55:32 +0100

    Added attributes to Employee class

--- src/POJO/Employee.java
+++ src/POJO/Employee.java
@@ -1,5 +1,41 @@
 package POJO;
 
-
 public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
