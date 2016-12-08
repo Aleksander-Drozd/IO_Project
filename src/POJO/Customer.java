a184e30ad6a94f23c6f165f16ba7c27275ffeb8d
bumpMind <damian.s.ja@gmail.com>
2016-12-08 18:11:14 +0100

    Added attributes to Customer POJO

--- src/POJO/Customer.java
+++ src/POJO/Customer.java
@@ -2,4 +2,59 @@ package POJO;
 
 
 public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String street;
+    private String postCode;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
