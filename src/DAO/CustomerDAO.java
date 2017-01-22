b33404e3223148a6973d690cfab547ff915741ca
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-01-22 15:55:49 +0100

    Implemented CustomerDAO

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -1,4 +1,48 @@
 package DAO;
 
+import POJO.Customer;
+import Util.DatabaseUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CustomerDAO {
+    public static Customer getCustomer(int id) {
+        Customer customer = null;
+        ResultSet resultSet;
+
+        String query = "SELECT * FROM customers WHERE id='" + id + "';";
+
+        resultSet = DatabaseUtil.runStatement(query);
+        try {
+            if (resultSet.next()) {
+                customer = createCustomer(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customer;
+    }
+
+    private static Customer createCustomer(ResultSet resultSet) throws SQLException{
+        Customer customer = new Customer();
+        int gender;
+
+        customer.setFirstName(resultSet.getString("first_name"));
+        customer.setLastName(resultSet.getString("last_name"));
+        customer.setCity(resultSet.getString("city"));
+        customer.setStreet(resultSet.getString("street"));
+        customer.setPostCode(resultSet.getString("post_code"));
+        customer.setPhoneNumber(resultSet.getString("phone_number"));
+
+        gender = resultSet.getInt("gender");
+
+        if (gender == 1)
+            customer.setGender("Male");
+        else
+            customer.setGender("Female");
+
+        return customer;
+    }
 }
