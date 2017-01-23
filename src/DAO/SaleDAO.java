7dc9b206cfdbb102f62ce79ef3deb5d39f85307a
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-01-23 13:04:29 +0100

    Added method to SaleDAO enabling adding new sale to database

--- src/DAO/SaleDAO.java
+++ src/DAO/SaleDAO.java
@@ -59,4 +59,40 @@ public class SaleDAO {
 
         return sale;
     }
+
+    public static boolean addSale(Sale sale){
+        int gender, customerId;
+        Customer customer = sale.getCustomer();
+
+        if (customer.getGender().equals("male"))
+            gender = 1;
+        else
+            gender = 0;
+
+        String insertCustomerQuery = "INSERT INTO customers (first_name, last_name, gender, city, street, post_code, phone_number)" + "VALUES ('" +
+                customer.getFirstName() + "', '" +
+                customer.getLastName() + "', '" +
+                gender + "', '" +
+                customer.getCity() + "', '" +
+                customer.getStreet() + "', '" +
+                customer.getPostCode() + "', '" +
+                customer.getPhoneNumber() + "');";
+
+        customerId = DatabaseUtil.update(insertCustomerQuery, DatabaseUtil.START_TRANSACTION);
+
+        if (customerId == -1)
+            return false;
+
+        String insertSaleQuery = "INSERT INTO sales (employee_id, trip_id, customer_id, quantity, date) VALUES ('" +
+                EmployeeDAO.getLoggedEmployee().getId() + "', '" +
+                sale.getTrip().getId() + "', '" +
+                customerId + "', '" +
+                sale.getQuantity() + "', '" +
+                sale.getSaleDate() + "');";
+
+        if (DatabaseUtil.update(insertSaleQuery, DatabaseUtil.END_TRANSACTION) == -1)
+            return false;
+
+        return true;
+    }
 }
