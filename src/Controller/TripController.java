268db750e263b335484cc339505ab308ae80db85
bumpMind <damian.s.ja@gmail.com>
2017-02-13 11:34:20 +0100

    Connected field in View with variables in Controller

--- src/Controller/TripController.java
+++ src/Controller/TripController.java
@@ -2,12 +2,28 @@ package Controller;
 
 
 import POJO.Trip;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TripController implements Initializable {
+
+    @FXML
+    private TextField title;
+    @FXML
+    private TextArea description;
+    @FXML
+    private TextField days;
+    @FXML
+    private TextField price;
+    @FXML
+    private DatePicker date;
+
 
     private Trip trip;
 
@@ -17,6 +33,5 @@ public class TripController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
