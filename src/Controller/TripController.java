523871767694412816718c4ba12fefedeb020b1d
bumpMind <damian.s.ja@gmail.com>
2017-02-13 11:56:03 +0100

    Show got Trip data on TripView

--- src/Controller/TripController.java
+++ src/Controller/TripController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.time.ZoneId;
 import java.util.ResourceBundle;
 
 public class TripController implements Initializable {
@@ -24,14 +25,28 @@ public class TripController implements Initializable {
     @FXML
     private DatePicker date;
 
-
     private Trip trip;
 
-    public void setTrip(Trip trip) {
-        this.trip = trip;
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+
+        showTripData();
+    }
+
+    private void showTripData() {
+        int tripDays = trip.getDays();
+        float tripPrice = trip.getPrice();
+
+        title.setText( trip.getTitle() );
+        description.setText( trip.getDescription() );
+        days.setText( tripDays != 0 ? Integer.toString(tripDays) : "" );
+        price.setText( tripPrice != 0.0f ? Float.toString(tripPrice) : "" );
+        date.setValue( trip.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate() );
+    }
+
 }
