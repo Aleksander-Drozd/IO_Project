07add2fe60ce296e1ee2e00a1c8ea291d7290d2c
bumpMind <damian.s.ja@gmail.com>
2017-02-13 14:01:48 +0100

    Added simple validate methods

--- src/Controller/TripController.java
+++ src/Controller/TripController.java
@@ -4,12 +4,13 @@ package Controller;
 import POJO.Trip;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.time.DateTimeException;
+import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class TripController implements Initializable {
@@ -26,16 +27,11 @@ public class TripController implements Initializable {
     private DatePicker date;
 
     private Trip trip;
+    private boolean correctData = true;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    }
-
-    public void setTrip(Trip trip) {
-        this.trip = trip;
-
-        showTripData();
     }
 
     private void showTripData() {
@@ -47,6 +43,135 @@ public class TripController implements Initializable {
         days.setText( tripDays != 0 ? Integer.toString(tripDays) : "" );
         price.setText( tripPrice != 0.0f ? Float.toString(tripPrice) : "" );
         date.setValue( trip.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate() );
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+
+        showTripData();
+    }
+
+    @FXML
+    private void saveTrip() {
+        validateData();
+
+        if (correctData) {
+
+        }
+    }
+
+    @FXML
+    private void closeView() {
+        System.out.println("Close view!");
+    }
+
+    private void validateData() {
+        correctData = true;
+
+        validateTitle();
+        validateDescription();
+        validatePrice();
+        validateDays();
+        validateDate();
+    }
+
+
+    private void validateTitle() {
+        if ( isEmpty(title) ) {
+            addErrorCSSClass(title);
+            correctData = false;
+        } else {
+            removeErrorCSSClass(title);
+        }
+    }
+
+    private void validateDescription() {
+        if ( isEmpty(description) ) {
+            addErrorCSSClass(description);
+            correctData = false;
+        } else {
+            removeErrorCSSClass(description);
+        }
+    }
+
+    private void validateDays() {
+        if ( isEmpty(days) ) {
+            addErrorCSSClass(days);
+            correctData = false;
+        } else {
+            if ( isCorrectIntegerNumber(days) ) {
+                removeErrorCSSClass(days);
+            } else {
+                addErrorCSSClass(days);
+                correctData = false;
+            }
+        }
+    }
+
+    private void validatePrice() {
+        if ( isEmpty(price) ) {
+            addErrorCSSClass(price);
+            correctData = false;
+        } else {
+            if ( isCorrectFloatingPointNumber(price) ) {
+                removeErrorCSSClass(price);
+            } else {
+                addErrorCSSClass(price);
+                correctData = false;
+            }
+        }
+    }
+
+    private void validateDate() {
+        if ( isCorrectDate(date) ) {
+            removeErrorCSSClass(date);
+        } else {
+            addErrorCSSClass(date);
+        }
+    }
+
+
+    private void removeErrorCSSClass(Control field) {
+        field.getStyleClass().remove("textfield-error");
+        field.getStyle();
+    }
+
+    private void addErrorCSSClass(Control field) {
+        field.getStyleClass().add("textfield-error");
+    }
+
+    private boolean isCorrectDate(DatePicker date) {
+        try {
+            Date.from( Instant.from( date.getValue().atStartOfDay( ZoneId.systemDefault() ) ) );
+
+            return true;
+        } catch (DateTimeException | IllegalArgumentException | NullPointerException e) {
+            return false;
+        }
+    }
+
+    private boolean isCorrectIntegerNumber(TextField field) {
+        try {
+            Integer.parseInt(field.getText());
+
+            return true;
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
+    }
+
+    private boolean isCorrectFloatingPointNumber(TextField field) {
+        try {
+            Float.parseFloat(field.getText());
+
+            return true;
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
+    }
+
+    private boolean isEmpty(TextInputControl field) {
+        return field.getText().isEmpty();
     }
 
 }
