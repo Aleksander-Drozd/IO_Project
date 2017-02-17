b95c9199d87ff7145488c1960fc141b96b8cdcce
bumpMind <damian.s.ja@gmail.com>
2017-02-17 14:51:42 +0100

    Added ChartModel

---
+++ src/Model/ChartModel.java
@@ -0,0 +1,22 @@
+package Model;
+
+
+import javafx.scene.chart.Chart;
+
+import java.util.List;
+
+public class ChartModel {
+
+    public Chart generatePieChart(List<Object> dataForChart) {
+        return null;
+    }
+
+    public Chart generateLineChart(List<Object> dataForChart) {
+        return null;
+    }
+
+    public Chart generateBarChart(List<Object> dataForChart) {
+        return null;
+    }
+
+}
