033c12d510d22666a3f79bc4b093fbe8b6b662c3
bumpMind <damian.s.ja@gmail.com>
2017-02-17 18:59:46 +0100

    Connected fields from StatisticsView to Controller

--- src/Controller/StatisticsController.java
+++ src/Controller/StatisticsController.java
@@ -5,15 +5,34 @@ import Util.ChartDataEntity;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-
 public class StatisticsController {
+
+
+    @FXML
+    private ListView<?> chartSchemesList;
+    @FXML
+    private TextField numberOfScopes;
+    @FXML
+    private ChoiceBox<?> scopeType;
+    @FXML
+    private TextField numberOfAccuracy;
+    @FXML
+    private ChoiceBox<?> accuracyType;
+    @FXML
+    private RadioButton barChart;
+    @FXML
+    private RadioButton lineChart;
+    @FXML
+    private RadioButton pieChart;
 
     private StatisticModel statisticModel;
 
