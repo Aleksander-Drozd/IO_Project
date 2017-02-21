d9bb2fa4e0bead1e1d9999da41b63dcb527878dc
bumpMind <damian.s.ja@gmail.com>
2017-02-21 09:00:01 +0100

    Added interface ChartConfigModel

---
+++ src/Model/ChartConfig/ChartConfigModel.java
@@ -0,0 +1,13 @@
+package Model.ChartConfig;
+
+
+import Util.ChartDataEntity;
+import javafx.collections.ObservableList;
+
+import java.util.Map;
+
+public interface ChartConfigModel {
+
+    public ObservableList<ChartDataEntity> getDataForChart(Map config);
+
+}
