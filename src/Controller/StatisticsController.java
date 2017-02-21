98f8d36062545be451c9dcec8d4ce54d2322654d
bumpMind <damian.s.ja@gmail.com>
2017-02-21 09:06:01 +0100

    Updated statistic model and controller

--- src/Controller/StatisticsController.java
+++ src/Controller/StatisticsController.java
@@ -1,47 +1,89 @@
 package Controller;
 
+import Controller.ChartConfig.ChartConfigController;
 import Model.StatisticModel;
 import Util.ChartDataEntity;
+import Util.ChartType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-public class StatisticsController {
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class StatisticsController implements Initializable {
 
 
     @FXML
-    private ListView<?> chartSchemesList;
+    private ListView<String> chartSchemesList;
     @FXML
-    private TextField numberOfScopes;
+    private Pane chartConfig;
     @FXML
-    private ChoiceBox<?> scopeType;
+    private RadioButton barChartRadioButton;
     @FXML
-    private TextField numberOfAccuracy;
+    private RadioButton lineChartRadioButton;
     @FXML
-    private ChoiceBox<?> accuracyType;
-    @FXML
-    private RadioButton barChart;
-    @FXML
-    private RadioButton lineChart;
-    @FXML
-    private RadioButton pieChart;
+    private RadioButton pieChartRadioButton;
 
+    private ToggleGroup chartTypeToggleGroup;
+    private ChartType chartType;
+
+    private ChartConfigController chartConfigController;
     private StatisticModel statisticModel;
+
+    public StatisticsController() {
+        chartTypeToggleGroup = new ToggleGroup();
+        statisticModel = new StatisticModel();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initChartTypeToggleGroup();
+
+        chartSchemesList.setItems(
+                FXCollections.observableArrayList("Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise")
+        );
+
+        chartSchemesList.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showChartConfigPane(newValue)
+        );
+
+        chartSchemesList.getSelectionModel().select(0);
+    }
+
+    private void initChartTypeToggleGroup() {
+        barChartRadioButton.setToggleGroup(chartTypeToggleGroup);
+        lineChartRadioButton.setToggleGroup(chartTypeToggleGroup);
+        pieChartRadioButton.setToggleGroup(chartTypeToggleGroup);
+
+        chartTypeToggleGroup.selectedToggleProperty().addListener((ov, toggle, new_toggle) -> {
+            if (new_toggle == barChartRadioButton) {
+                chartType = ChartType.BAR_CHART;
+            } else if (new_toggle == lineChartRadioButton) {
+                chartType = ChartType.LINE_CHART;
+            } else if (new_toggle == pieChartRadioButton) {
+                chartType = ChartType.PIE_CHART;
+            }
+        });
+    }
 
     @FXML
     private void handleChartButton(){
-
-
-        //List<ChartDataEntity> list = statisticModel.getDataForChart(  );
-
+        System.out.print("");
+        Map config = chartConfigController.getChartConfig();
+        config.put("type", chartType.toString());
+        ObservableList<ChartDataEntity> list = statisticModel.getDataForChart(config);
 
         Stage dataSaleStage = new Stage();
 
@@ -64,4 +106,18 @@ public class StatisticsController {
         }
     }
 
+    private void showChartConfigPane(String newValue) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../View/SimpleChartConfig.fxml"));
+
+            AnchorPane anchorPane = loader.load();
+
+            chartConfigController = loader.getController();
+
+            chartConfig.getChildren().add( anchorPane );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Model/StatisticModel.java
+++ src/Model/StatisticModel.java
@@ -1,5 +1,11 @@
 package Model;
 
+import Model.ChartConfig.ChartConfigModel;
+import Model.ChartConfig.SalesSalesmanChartModel;
+import Util.ChartDataEntity;
+import Util.ChartTemplate;
+import Util.ChartType;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import DAO.EmployeeDAO;
 import DAO.SaleDAO;
@@ -7,6 +13,8 @@ import DAO.TripDAO;
 import POJO.Employee;
 import POJO.Sale;
 import POJO.Trip;
+
+import java.util.Map;
 
 public class StatisticModel {
 
@@ -18,6 +26,8 @@ public class StatisticModel {
     private SaleDAO saleDAO;
     private EmployeeDAO employeeDAO;
 
+    private ChartConfigModel chartConfigModel;
+
     public StatisticModel() {
         tripDAO = new TripDAO();
         saleDAO = new SaleDAO();
@@ -25,4 +35,19 @@ public class StatisticModel {
     }
 
 
+    public ObservableList<ChartDataEntity> getDataForChart(Map config) {
+        ObservableList<ChartDataEntity> chartData = FXCollections.observableArrayList();
+
+        ChartTemplate chartTemplate = ChartTemplate.valueOf( (String) config.get("template") );
+
+        switch (chartTemplate) {
+            case SALES_SALESMAN:
+                chartConfigModel = new SalesSalesmanChartModel();
+                break;
+        }
+
+        chartConfigModel.getDataForChart(config);
+
+        return chartData;
+    }
 }
