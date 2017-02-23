887f79be2bd06ba6bc0c42c30d23a19c8369c968
bumpMind <damian.s.ja@gmail.com>
2017-02-23 12:29:00 +0100

    Added implementation getDataForChart and clear method getEmployeeSalesCount

--- src/Model/ChartConfig/SalesSalesmanChartModel.java
+++ src/Model/ChartConfig/SalesSalesmanChartModel.java
@@ -1,27 +1,48 @@
 package Model.ChartConfig;
 
 
+import DAO.EmployeeDAO;
 import DAO.SaleDAO;
+import POJO.Employee;
 import Util.ChartDataEntity;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.List;
 import java.util.Map;
 
 public class SalesSalesmanChartModel implements ChartConfigModel {
 
     private SaleDAO saleDAO;
+    private EmployeeDAO employeeDAO;
 
     public SalesSalesmanChartModel() {
         saleDAO = new SaleDAO();
-        saleDAO.getSales();
+        employeeDAO = new EmployeeDAO();
     }
 
     @Override
     public ObservableList<ChartDataEntity> getDataForChart(Map config) {
         String conf = (String) config.get("scopeMultiplier");
 
+        ObservableList<ChartDataEntity> chartDataEntities = FXCollections.observableArrayList();
 
-        return null;
+        List<Employee> employees = employeeDAO.getEmployees();
+        employees.forEach( employee -> {
+            int count = getEmployeeSalesCount(employee, config);
+            ChartDataEntity entity = new ChartDataEntity(employee.toString(), count);
+            chartDataEntities.add(entity);
+        });
+
+        return chartDataEntities;
+    }
+
+    private int getEmployeeSalesCount(Employee employee, Map config) {
+        int count = 0;
+
+        System.out.println( "Employee id: " + employee.getId() );
+
+        return count;
     }
 
 }
