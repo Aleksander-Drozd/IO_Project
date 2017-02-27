0b915a9cdf4adaf5e8291dca369516ef71b9efbe
Aleksander Drozd <aleksander.drozd@outlook.com>
2017-02-27 19:54:54 +0100

    Added tests for valid addresses

--- Tests/Util/ValidatorTest.java
+++ Tests/Util/ValidatorTest.java
@@ -8,7 +8,7 @@ import static Util.Validator.*;
 public class ValidatorTest {
 
     @Test
-    public void postCode_validStrings() throws Exception{
+    public void postCode_validStrings() throws Exception {
         String text;
 
         text = "23-459";
@@ -25,7 +25,7 @@ public class ValidatorTest {
     }
 
     @Test
-    public void postCode_corruptedStrings() throws Exception{
+    public void postCode_corruptedStrings() throws Exception {
         String text;
 
         text = "a2-456";
@@ -57,5 +57,46 @@ public class ValidatorTest {
 
         text = "ff-dfs";
         assertFalse(text.matches(POST_CODE_REGEX));
+    }
+
+    @Test
+    public void address_validStrings() throws Exception {
+        String text;
+
+        text = "Sportowa 1";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Kartuska 50";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Sapiehy Jana 106";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Orkana Władysława 80";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Beskidzka 15/4";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Al. Majowa 53";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Wybrzeże Stanisława Wyspiańskiego 21";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Norweska 81/4/34";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Warszawska 12a/4";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Poznańska 11/4a/45";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Wrocławska 11d/4a/55";
+        assertTrue(text.matches(ADDRESS_REGEX));
+
+        text = "Krakowska 11c/4/21";
+        assertTrue(text.matches(ADDRESS_REGEX));
     }
 }
