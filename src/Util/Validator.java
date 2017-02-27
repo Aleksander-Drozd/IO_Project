418cbb4829782fce61663cd9058a2bb17cef7d95
bumpMind <damian.s.ja@gmail.com>
2017-02-27 17:20:21 +0100

    Added class Validator with static regex for validate

---
+++ src/Util/Validator.java
@@ -0,0 +1,12 @@
+package Util;
+
+
+public class Validator {
+
+    public static final String POST_CODE_REGEX = "^\\d{2}-\\d{3}$";
+
+    public static final String ADDRESS_REGEX = "^[a-zA-Z]{2,}([a-zA-Z]{0,2})?(/\\d{1,4}([a-zA-Z]{0,2})?){0,2}$";
+
+    public static final String PHONE_NUMBER_REGEX = "^(\\d{3}( |-)){2}\\d{3}$";
+
+}
